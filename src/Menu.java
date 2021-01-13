a8cee1948b7b94bd3d9ae1603a6bb427f220b3e9
Clement Saw Kiat Chun <cskiat06@gmail.com>
2021-01-13 12:37:32 +0800

    Rename methods

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Menu {
+    private ArrayList<Beverage> coffeeMenu;
+    private ArrayList<Beverage> condimentsMenu;
+
+    //Add coffee into coffees ArrayList
+    public void addCoffee(Beverage coffee){
+        coffeeMenu.add(coffee);
+    };
+
+    //Get coffees ArrayList
+    public ArrayList<Beverage> getCoffeeMenu(){
+        return coffeeMenu;
+    };
+
+
+    //Add condiment into condiments ArrayList
+    public void addCondiments(Beverage condiment){
+        condimentsMenu.add(condiment);
+    };
+
+    //Get condiments ArrayList
+    public ArrayList<Beverage> getCondimentsMenu(){
+        return condimentsMenu;
+    };
 
 }
