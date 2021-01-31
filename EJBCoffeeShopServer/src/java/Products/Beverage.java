bc5620401cce0bca228127c1076f76dfacb3b732
CarlosSia <siasimcheong@gmail.com>
2021-01-31 12:09:02 +0800

    Add @Entity at beverage class

--- EJBCoffeeShopServer/src/java/Products/Beverage.java
+++ EJBCoffeeShopServer/src/java/Products/Beverage.java
@@ -1,9 +1,14 @@
 package Products;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
-public abstract class Beverage {
+public abstract class Beverage implements Serializable {
+
+    @Id
+    private Long beverageId;
 
     protected String description;
     protected Double price;
@@ -13,4 +18,12 @@ public abstract class Beverage {
     }
 
     public abstract Double getPrice();
+
+    public Long getId() {
+        return beverageId;
+    }
+
+    public void setId(Long beverageId) {
+        this.beverageId = beverageId;
+    }
 }
